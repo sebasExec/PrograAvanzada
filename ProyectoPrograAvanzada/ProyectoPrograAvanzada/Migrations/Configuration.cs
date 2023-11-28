536db4db20ad363ad2341e53667b63e611101df5
Sebas <sbriones284@gmail.com>
2023-11-28 12:23:38 -0600

    Login

---
+++ ProyectoPrograAvanzada/ProyectoPrograAvanzada/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace ProyectoPrograAvanzada.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ProyectoPrograAvanzada.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "ProyectoPrograAvanzada.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(ProyectoPrograAvanzada.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ ProyectoPrograAvanzada/ProyectoPrograAvanzada/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProyectoPrograAvanzada.Startup))]
+namespace ProyectoPrograAvanzada
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
